9c6fb42a38ddee148a39932441c8f02e15e98808
Werner Lopes <150675012+wern7r@users.noreply.github.com>
2025-11-09 20:00:14 -0300

    ops

--- CourseFinalProject/Models/ViewsModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CourseFinalProject.Models.ViewsModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
